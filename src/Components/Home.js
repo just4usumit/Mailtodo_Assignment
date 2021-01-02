5ef656f1546f9dab6bfda458ee7378198b023b1f
Sumit Ranjan <just4usumit70@gmail.com>
2021-01-03 03:11:50 +0530

    Made Counters to Work

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -30,8 +30,16 @@ import {useState,useEffect} from 'react'
             }
           )
       }, [])
+      
+      function count_type(type){
+        let c = 
+        items.filter(function(item){
+            return item.current_status_code === type;
+          }).length
+        return c
+      }
     
-      //console.log(items)
+      console.log(items)
 
 
      return(
@@ -39,21 +47,22 @@ import {useState,useEffect} from 'react'
         <Navbar/>
         <div className="container mt-4 text-center">
         <div className="mr-4 d-inline">
-        <Counter type="DEL" count="30"  selected={true}/>
+        <Counter type="DEL" count={count_type("DEL")}  selected={true}/>
         </div>
         <div className="mr-4 d-inline">
-        <Counter type="INT" count="310" selected={false}/>
+        <Counter type="INT" count={count_type("INT")} selected={false}/>
         </div>
         <div className="mr-4 d-inline">
-        <Counter type="OOD" count="196"  selected={false}/>
+        <Counter type="OOD" count={count_type("OOD")}  selected={false}/>
         </div>
         <div className="mr-4 d-inline">
-        <Counter type="DEX" count="110"  selected={false}/>
+        <Counter type="DEX" count={count_type("UND")}  selected={false}/>
         </div>
         <div className="mr-4 d-inline">
-        <Counter type="NFI" count="100"  selected={false}/>
+        <Counter type="NFI" count={count_type("NFI")}  selected={false}/>
         </div>
         </div>
+        
       </div>
      )
  }
